af125236c34a5dfdd0bd07bf6bd7bf36f442443b
jiuhongxi <jiuhongxi@sina.com>
2019-08-26 01:17:05 +0800

    init commit

---
+++ src/pages/list/list.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div @click="search">点我搜索</div>
+    <div class="tabs">
+      <div class="tab" v-for="(item, index) in tabs" :key="index" :class="index==activeTab" @click="clickTab(index)">{{item}}</div>
+    </div>
+    <div v-for="(item, index) in productList" :key="index">index</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeTab: 0,
+      tabs: ['产险', '个险', '车险'],
+      productList: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
+  },
+  methods: {
+    search () {
+      console.log('点我搜索')
+    },
+    clickTab (tab) {
+      this.activeTab = tab
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tabs {
+    display: flex;
+  }
+  .tab {
+    flex: 1;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
